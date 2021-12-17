6d05a350fca2425b1254287e5ceb0ec1bae2caf7
pathenk <kontakt@phenkelmann.de>
2021-12-17 10:01:45 +0100

    init

---
+++ c204-app/web/Controller/InitController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InitController : ControllerBase
+    {
+
+  
+        private readonly IMemoryCache _memCache;
+        public InitController(IMemoryCache memCache)
+        {
+            _memCache = memCache;
+        }
+
+        // GET: api/<InitController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var isInit = _memCache.Get<bool>("INIT");
+            if (isInit)
+            {
+                return Ok("Already init");
+            }
+            _memCache.Set("INIT", true);
+            return Ok("Init finished");
+        }    
+
+    }
+}
