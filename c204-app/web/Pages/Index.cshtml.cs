c60aaf70a6c837f2905cb275ea858124d1de5e5b
pathenk <kontakt@phenkelmann.de>
2021-12-19 21:51:56 +0100

    blazor

--- c204-app/web/Pages/Index.cshtml.cs
+++ c204-app/web/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,16 +12,33 @@ namespace web.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<string> BlobNames = new List<string>();
         private readonly ILogger<IndexModel> _logger;
+        private readonly BlobServiceClient blobServiceClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BlobServiceClient blobServiceClient)
         {
             _logger = logger;
+            this.blobServiceClient = blobServiceClient;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-       
+         
+            BlobContainerClient blobContainerClient = this.blobServiceClient.GetBlobContainerClient("web-container");
+           
+            await foreach(BlobItem blobItem in blobContainerClient.GetBlobsAsync())
+            {
+                this.BlobNames.Add(blobItem.Name);
+            }
+           
+        }
+  
+        
+        public async Task UploadRandomBlob()
+        {
+            var file = System.IO.File.ReadAllText(@"template.tx");
+            
         }
     }
 }
--- c204-app/web/Startup.cs
+++ c204-app/web/Startup.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace web
             services.AddRazorPages();
             services.AddMemoryCache();
             services.AddLogging();
+
+            var blobConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            services.AddScoped<BlobServiceClient>(x => new BlobServiceClient(blobConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
