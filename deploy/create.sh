0016e0520cb34cbdbe8bc247d05b8fc9f33ca199
pathenk <kontakt@phenkelmann.de>
2021-12-19 13:07:58 +0100

    enable autoswap

--- deploy/create.sh
+++ deploy/create.sh
@@ -2,7 +2,7 @@
 
 location="germanywestcentral"
 rg="pathenk"
-as="appservice-pathenk"
+as="appservice-win-pathenk"
 
 # # Account set subscriptio
 
@@ -12,13 +12,16 @@ az group create --name $rg --location $location
 # # Create service plan
 az appservice plan create --name $as --resource-group $rg --sku s1 --is-linux 
     # \ --per-site-scaling # enable app scaling on app level
+# # Create service plan windows
+az appservice plan create --name "$as-win" --resource-group $rg --sku s1
+    # \ --per-site-scaling # enable app scaling on app level
 
 
-webappname="webapp-web"
+webappname="webapp-win-web"
 # # Create web app
 az webapp list-runtimes # list runtimes
 az webapp create --name $webappname --plan $as --resource-group $rg --runtime "DOTNET|5.0" \
     # --deployment-source-url https://github.com/pati1892/c204-app
 # Use deployment-source-url or
 az webapp deployment source config --name $webappname --resource-group $rg \
-    --repo-url https://github.com/pati1892/c204-app --branch master --manual-integration
+    --repo-url https://github.com/pati1892/c204-app --branch main --manual-integration
